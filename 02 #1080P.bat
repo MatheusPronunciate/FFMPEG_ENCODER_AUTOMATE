6ba625be9561c0c94d308ab34f2eebbd4388acce
Matheus Rodrigues Pronunciate <36574677+MatheusPronunciate@users.noreply.github.com>
2022-03-13 18:58:51 -0300

    Update 02 #1080P.bat

--- 02 #1080P.bat
+++ 02 #1080P.bat
@@ -1,1 +1,1 @@
-for %%a in ("*.*") do "ffmpeg\ffmpeg.exe" -i "%%a" -map_metadata -1 -c:v libx264 -profile:v main -level:v 4.1 -preset slow -crf 18 -c:a copy "newfiles\%%~na (1080p).mp4"
+for %%a in ("*.*") do "ffmpeg\ffmpeg.exe" -i "%%a" -c:v h264_amf -profile:v main -level:v 4.1 -rc 0 -qp_b 22 -qp_i 22 -qp_p 22 -quality 2 -c:a copy -c:s copy "newfiles\%%~na (1080p).mkv"
